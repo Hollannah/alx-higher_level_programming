ba4dba9a9589c41b495409a4f78d2f0334de3124
Olaide Hannah <118212140+Hollannah@users.noreply.github.com>
2023-08-06 09:07:54 +0100

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,20 +1,26 @@
 #!/usr/bin/node
-// Get the number of tasks completed by user id
+
 const request = require('request');
-request(process.argv[2], { json: true }, (err, resp, body) => {
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
   if (err) {
     console.log(err);
-  } else if (body) {
-    const tasksByUser = {};
-    for (const i in body) {
-      if (body[i].completed === true) {
-        if (tasksByUser[body[i].userId] === undefined) {
-          tasksByUser[body[i].userId] = 1;
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
         } else {
-          tasksByUser[body[i].userId]++;
+          completed[task.userId]++;
         }
       }
     }
-    console.log(tasksByUser);
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });

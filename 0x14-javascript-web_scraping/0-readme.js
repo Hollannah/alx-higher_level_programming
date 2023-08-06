1aeb1ba6b4e874a20169d4591cee6e0231fa327e
Olaide Hannah <118212140+Hollannah@users.noreply.github.com>
2023-08-06 09:04:06 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-// Print the contents of a file
+
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
   } else {

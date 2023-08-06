4c64ac85ac9ba6852da7e43fc90b3a6798b29e08
Olaide Hannah <118212140+Hollannah@users.noreply.github.com>
2023-08-06 09:06:04 +0100

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
-// Make a GET request and print the status code
+
 const request = require('request');
-request(process.argv[2], (err, resp, body) => {
+const URL = process.argv[2];
+
+request(URL, function (err, response) {
   if (err) {
     console.log(err);
-  } else if (resp) {
-    console.log(`code: ${resp.statusCode}`);
+  } else {
+    console.log('code: ' + response.statusCode);
   }
 });

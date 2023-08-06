866b7975dfca82a5ed2aed5cdf09e3779a3641d9
Olaide Hannah <118212140+Hollannah@users.noreply.github.com>
2023-08-06 09:04:35 +0100

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-// Write a string to a file
+
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
-  if (err) {
-    console.log(err);
-  }
+const file = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(file, string, 'utf-8', function (err) {
+  if (err) console.log(err);
 });
